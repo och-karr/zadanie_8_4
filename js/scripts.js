e61d284c41d30e8923cc4130f76f58b5c41f18bf
karolinneczka <okaa93@gmail.com>
2018-04-15 18:20:25 +0200

    Add files to repository

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var name = prompt('Enter your name'); //name - nazwa zmiennej, prompt funkcja z przekazaniem parametru
+//parametr zostaje wartością zmiennej
+// alert('Hello, ' + name); //podobna do prompt, ale nie posiada pola na wpisanie wartości
+
+/*to rowniez komentarz
+*/
+console.log('Hello, ' + name);
